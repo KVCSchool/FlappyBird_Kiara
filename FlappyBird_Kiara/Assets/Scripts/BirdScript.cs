aa7801e1e9b9d736932a6179e00b28089efd90a2
KVCSchool <kiano.van.cauter@ll.sintgregorius.be>
2025-09-16 14:22:04 +0200

    Player: configure gravity & flap strength

--- FlappyBird_Kiara/Assets/Scripts/BirdScript.cs
+++ FlappyBird_Kiara/Assets/Scripts/BirdScript.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     //TODO: Change to conform to expectations of teacher
+
     public Rigidbody2D RigidBody;
+
+    public float flapStrength;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +19,6 @@ public class BirdScript : MonoBehaviour
     {
         //FIXME: Switch to old input manager for this tutorial
         if (Input.GetKeyDown(KeyCode.Space))
-            RigidBody.linearVelocity = Vector2.up * 10.0f;
+            RigidBody.linearVelocity = Vector2.up * flapStrength;
     }
 }
