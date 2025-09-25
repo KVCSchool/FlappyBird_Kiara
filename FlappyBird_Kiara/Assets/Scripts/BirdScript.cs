70bece364343eebeb762f1ca44b5016cfea62f9d
KVCSchool <kiano.van.cauter@ll.sintgregorius.be>
2025-09-25 20:04:10 +0200

    Game: add game over & retry

--- FlappyBird_Kiara/Assets/Scripts/BirdScript.cs
+++ FlappyBird_Kiara/Assets/Scripts/BirdScript.cs
@@ -4,21 +4,29 @@ public class BirdScript : MonoBehaviour
 {
     //TODO: Change to conform to expectations of teacher
 
-    public Rigidbody2D RigidBody;
+    public bool isAlive = true;
 
     public float flapStrength;
+
+    public Rigidbody2D RigidBody;
+    private LogicScript logic;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //FIXME: Switch to old input manager for this tutorial
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isAlive)
             RigidBody.linearVelocity = Vector2.up * flapStrength;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isAlive = false;
+        logic.GameOver();
     }
 }
--- FlappyBird_Kiara/Assets/Scripts/LogicScript.cs
+++ FlappyBird_Kiara/Assets/Scripts/LogicScript.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class LogicScript : MonoBehaviour
     public int playerScore;
 
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase score")]
     public void AddScore(int val)
@@ -15,5 +17,15 @@ public class LogicScript : MonoBehaviour
         playerScore += val;
 
         scoreText.text = playerScore.ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
